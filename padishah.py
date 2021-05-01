a5ee0cced92003b6240a5dab392f1f8fae82898d
MadhavChoudhary <madhavtummala@icloud.com>
2021-05-01 19:04:04 +0530

    working code without encode

--- padishah.py
+++ padishah.py
@@ -1,34 +1,76 @@
-from flask import Flask, render_template
-import scheduler as Scheduler
+from flask import Flask, redirect, render_template, request
+from signal import signal, SIGTERM, SIGINT, SIGKILL
+import subprocess
 import threading
 import time
 
+""" global variables """
+wait_list = []
+is_running = False
+picket_item = []
+profile_list = []
+processed = 0
+total = 10
+""" global variables """
+
 app = Flask(__name__)
 @app.route("/")
-# show table with full waitlist or fist row running and waitlist
-# show form with foldername (blank), profile (selector)
 def home():
-  return render_template('index.html')
+    if is_running:
+        wait_list[0][2] = "{}/{}".format(processed, total)
+    return render_template('index.html', display_list=wait_list, profiles=profile_list)
 
-@app.route("/about")
-def about():
-  return render_template("about.html")
-
-# submit button in the home page form calls this
-# post request json, folder name, profile name
-@app.route('/add-queue-item')
+@app.route('/add-queue-item',methods=["GET","POST"])
 def add_queue_item():
-    Scheduler.add_waitlist_item()
-    return
+    if request.method == 'POST':
+        folder = request.form.get("fname")
+        profile = request.form.get("profile")
+        cron_thread_lock.acquire()
+        wait_list.append([folder, profile, "waiting"])
+        cron_thread_lock.release()
+    return redirect("/")
 
 def cron_pickup():
-    if not Scheduler.get_run_status():
-        Scheduler.run_new_item()
-    time.sleep(1000)
+    global is_running
+    global wait_list
+    global picket_item
+    global processed
+    while True:
+        if not is_running and len(wait_list)>0:
+            print("cron: picked up")
+            processed = 0
+            is_running = True
+            for i in range(total):
+                print("cron: processed {}".format(processed))
+                time.sleep(5)
+                processed += 1
+            if wait_list[0][2] != "failed":
+                print("cron: moving files to main dir")
+            else:
+                print("cron: keeping the incomplete files as they are")
+            cron_thread_lock.acquire()
+            wait_list.pop(0)
+            cron_thread_lock.release()
+            is_running = False
+            print("cron: pick complete")
+
+def cleanup(signal, frame):
+    print("doing cleanup")
+    if is_running:
+        print("cleaning up incomplete encode")
+    exit(0)
+
+signal(SIGTERM, cleanup)
+signal(SIGINT, cleanup)
+
+cron_thread = threading.Thread(target=cron_pickup, daemon=True)
+cron_thread.start()
+cron_thread_lock = threading.Lock()
+
+# load any queue items from persisting storage
+
+# load profiles
+profile_list = ["anime720p", "anime1080p"]
 
 if __name__ == '__main__':
-    Scheduler.init()
-    flask_thread = threading.Thread('t1', target=app.run(debug = True))
-    scheduler_thread = threading.Thread('t2', target=cron_pickup())
-    flask_thread.start()
-    scheduler_thread.start()
+    app.run()
